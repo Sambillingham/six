a0f8b1e027394644a3ca63970c6ae860228c3b06
Sam Billingham <sambillie.sb@gmail.com>
2013-02-22 13:37:41 +0000

    changed server to show all logs again

--- node/server.js
+++ node/server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server, { log: false }),
+    io = require('socket.io').listen(server, { log: true }),
     mqtt = require('mqttjs'),
     relationships = '';
     changes = '';
